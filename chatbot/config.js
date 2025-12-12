6d1b0b5558b6372cc245cf67aa6a06d9bf3418ee
paginasperu <josemuro.org@gmail.com>
2025-12-12 01:56:04 -0500

    Update config.js

--- chatbot/config.js
+++ chatbot/config.js
@@ -12,7 +12,7 @@ window.CHAT_CONFIG = {
             nombre: "Gemini (Gratis)",
             tipo: "google",
             // Pega aquí tu llave de Google (Restringida en Cloud Console)
-            apiKey: "AIzaSy_TU_CLAVE_DE_GOOGLE", 
+            apiKey: "AIzaSyAO-BPm8_6pY1bAW38kYYtnEEQGcQUAme4", 
             modelo: "gemini-2.5-flash"
         },
         {
